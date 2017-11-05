52db09e9a36716118439bebf7566bfa49f92b686
Chris Reid <chris.reid@akqa.com>
2017-11-05 22:12:16 +0000

    Refactored common code in controller

--- api/controllers/carsController.js
+++ api/controllers/carsController.js
@@ -3,52 +3,45 @@
 const mongoose = require('mongoose');
 const Car = mongoose.model('Cars');
 
+const responseHandler = (response, error, car) => {
+    if (error) {
+        response.send(error);
+    } else {
+        response.json(car);
+    }
+};
+
+// TODO: Could modify/add methods for creating, updating or deleting multiple items
+
 // Create or POST
 exports.createItem = (req, res) => {
     const car = new Car(req.body);
 
     console.log('request', req.body);
 
-    car.save((err, car) => {
-        console.log(car);
-        if (err) {
-            res.send(err);
-        } 
-
-        res.json(car);
+    car.save((error, car) => {
+        responseHandler(res, error, car);
     });
 };
 
 // Read or GET
 exports.readAllItems = (req, res) => {
-    Car.find({}, (err, car) => {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(car);
+    Car.find({}, (error, car) => {
+        responseHandler(res, error, car);
     });
 };
  
 exports.readItem = (req, res) => {
-    Car.findById(req.params.id, (err, car) => {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(car);
+    Car.findById(req.params.id, (error, car) => {
+        responseHandler(res, error, car);
     });
 };
 
 // Update or PUT
 exports.updateItem = (req, res) => {
-    console.log('PUT', Number.isInteger(req.body.topSpeed));
-    Car.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, (err, car) => {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(car);
+    console.log('PUT', req.body);
+    Car.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, (error, car) => {
+        responseHandler(res, error, car);
     });
 };
 
@@ -56,11 +49,13 @@ exports.updateItem = (req, res) => {
 exports.deleteItem = (req, res) => {
     Car.remove({
         _id: req.params.id
-    }, (err, car) => {
-        if (err) {
-            res.send(err);
+    }, (error, car) => {
+
+        if (error) {
+            res.send(error);
+        } else {
+            res.json({ message: 'deleted' });
         }
 
-        res.json({ message: 'deleted' });
     });
 };
