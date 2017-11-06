98f8d66ecf7723517553d6abc22c5266045a9674
Chris Reid <chris.reid@akqa.com>
2017-11-06 07:57:32 +0000

    * Added double number formatting/storing * Fine tuning mongoose schema validation

--- api/models/carsModel.js
+++ api/models/carsModel.js
@@ -3,36 +3,55 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const setDouble = (num) => {
+    return (num.toFixed(2)) * 100;
+};
+
+const getDouble = (num) => {
+    return (num / 100).toFixed(2);
+};
+
 const carSchema = new Schema({
     manufacturer: {
-        type: String,
-        required: 'Enter manufacturer name'
+        type     : String,
+        required : 'Enter manufacturer name'
     },
     model: {
-        type: String,
-        required: 'Enter car model'
+        type     : String,
+        required : 'Enter car model'
     },
     price: {
-        type     : Number
+        type     : Number,
+        min      : 0,
+        required : true,
+        get      : getDouble,
+        set      : setDouble
     },
     acceleration: {
-        type     : Number
+        type     : Number,
+        min      : 0,
+        required : true,
+        get      : getDouble,
+        set      : setDouble
     },
     topSpeed: {
         type     : Number,
         required : true,
         validate : {
+            isAsync   : true,
             validator : Number.isInteger,
             message   : '{VALUE} is not an integer value'
         }
     },
     isSold: {
-        type: Boolean,
-        default: false
+        type     : Boolean,
+        default  : false,
+        required : true
     },
     createdAt: {
-        type: Date,
-        default: Date.now
+        type     : Date,
+        default  : Date.now,
+        required : true
     }
 });
 
