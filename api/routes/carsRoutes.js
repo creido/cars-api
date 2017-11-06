4b26a1855bed24bc74c44eb6817b28ba3cf7aa75
Chris Reid <chris.reid@akqa.com>
2017-11-06 07:59:01 +0000

    Added homepage route and view

--- api/routes/carsRoutes.js
+++ api/routes/carsRoutes.js
@@ -4,6 +4,10 @@ module.exports = (app) => {
     const controller = require('../controllers/carsController');
 
     // set up routes
+    app.route('/').get((req, res) => {
+        res.render('index');
+    });
+
     app.route('/cars')
         .get(controller.readAllItems)
         .post(controller.createItem);
